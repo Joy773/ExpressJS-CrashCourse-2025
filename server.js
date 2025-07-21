f8e1e660f025081b2a42e2ae8412896f8b1d763b
Joy773 <dibbojitdasjoy@gmail.com>
2025-07-21 13:34:28 +0600

    Initial commit

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
+import dotenv from "dotenv";
 import express from 'express';
+import cors from 'cors';
+import {connectDB} from './src/config/db.js'
+import userRouter from './src/user/user.route.js';
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+//Database Connection
+try {
+    connectDB();
+    console.log('Connected to DB!')
+}
+catch (err)
+{
+    console.error(err);
+    process.exit(1);
+}
+app.use(cors());
+app.use(express.json()); // --> Global Middleware
+
+const reqLogger = (req, res, next) => {
+    console.log(`${req.method} ${req.url} ${new Date().toISOString()}`);
+    next();
+}
+
+app.use(reqLogger);
+app.use('/api/users',userRouter);
+
+
+
+//Register Routes
+
+
 app.get("/health", (req, res) => {
-    // res.status(200).json({message: "All good"});
-    // res.send("Hey! I am healthy");
+    res.send("Hey! I am healthy");
 })
 
 
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({message: err.message });
+})
 app.listen(PORT, () => console.log(`Server is listening on ${PORT}`))
---
+++ src/user/user.route.js
@@ -0,0 +1,57 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
+import { User } from "./user.mode.js";
+
+const router = express.Router();
+
+router.get('/:userId', (req, res, next) => {
+    res.json({});
+})
+//api/users
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    email,
+  });
+  if (!user) {
+    const error = new Error("Invalid Credentials");
+    error.statusCode = 400;
+    next(error);
+    return;
+  }
+  const matched = bcrypt.compareSync(password, user.password);
+  if (!matched) {
+    const error = new Error("Invalid Credentials");
+    error.statusCode = 400;
+    next(error);
+    return;
+  }
+  const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY, {expiresIn: 60 * 60});
+  res.json({token});
+});
+
+router.post("/", async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = new Error("all fields are required!");
+    error.statusCode = 400;
+    next(error);
+    // res.status(400).json({message: "All fields are required!"});
+    // return;
+  }
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const result = await User.create({
+      name,
+      email,
+      password: hash,
+    });
+    res.status(201).json({ id: result._id });
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
